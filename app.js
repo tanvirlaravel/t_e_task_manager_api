198b3bff8e6cb5b7d08f5e7155b6ef32252659d3
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-12-05 13:59:06 +0600

    18 done (do again)

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-require("./db/connect");
 const express = require("express");
 const app = express();
 const port = 3000;
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
 
 // middleware
 app.use(express.json());
@@ -12,6 +12,15 @@ app.get("/hello", (req, res) => {
   res.send("Task manager app");
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://127.0.0.1:${port}/`);
-});
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Server running on http://127.0.0.1:${port}/`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -2,16 +2,13 @@ const mongoose = require("mongoose");
 
 const connectionString = `mongodb+srv://tanvirlaravel:SifA4lmTmc54sHxM@cluster0.dig3zsv.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority`;
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log(`Connect to db...`);
-  })
-  .catch((err) => {
-    console.log(err);
   });
+};
+
+module.exports = connectDB;
