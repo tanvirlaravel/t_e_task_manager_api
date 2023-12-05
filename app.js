0272cb1a2045d03007d0b1f6122cefb189633037
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-12-05 12:13:13 +0600

    6 done

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
+app.use("/api/v1/tasks", tasks);
 
 app.get("/hello", (req, res) => {
   res.send("Task manager app");
