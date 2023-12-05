18feafbe459545543700606b5f9b556f6ade249a
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-12-05 11:53:06 +0600

    3 done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/hello", (req, res) => {
+  res.send("Task manager app");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://127.0.0.1:${port}/`);
+});
