9c42fdb36d73d7bdcb2a74407785160cbf19c328
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-12-05 13:51:38 +0600

    17 done (do again)

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,17 @@
-const connectionString = `mongodb+srv://tanvirlaravel:SifA4lmTmc54sHxM@cluster0.dig3zsv.mongodb.net/tasksManger?retryWrites=true&w=majority`;
+const mongoose = require("mongoose");
+
+const connectionString = `mongodb+srv://tanvirlaravel:SifA4lmTmc54sHxM@cluster0.dig3zsv.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(connectionString, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Connect to db...`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
