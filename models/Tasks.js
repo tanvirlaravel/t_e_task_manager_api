efe2988ebae4f351c4f2d33f81721dff13e18bf7
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-12-05 16:14:38 +0600

    23  done)

--- models/Tasks.js
+++ models/Tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    maxlength: [20, "name can not be more then 20 charecters"],
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
